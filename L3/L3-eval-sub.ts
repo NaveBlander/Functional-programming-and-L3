48f9a1b5dd1a926db00a42e78a30901b5aadb9f4
Nave Blander <nave8520@gmail.com>
2024-06-17 15:41:15 +0300

    2a completed

--- L3/L3-eval-sub.ts
+++ L3/L3-eval-sub.ts
@@ -1,6 +1,6 @@
 // L3-eval.ts
 import { map } from "ramda";
-import { isCExp, isLetExp, isClassExp, ClassExp, makeClassExp} from "./L3-ast";
+import { isCExp, isLetExp } from "./L3-ast";
 import { BoolExp, CExp, Exp, IfExp, LitExp, NumExp,
          PrimOp, ProcExp, Program, StrExp, VarDecl } from "./L3-ast";
 import { isAppExp, isBoolExp, isDefineExp, isIfExp, isLitExp, isNumExp,
@@ -8,7 +8,7 @@ import { isAppExp, isBoolExp, isDefineExp, isIfExp, isLitExp, isNumExp,
 import { makeBoolExp, makeLitExp, makeNumExp, makeProcExp, makeStrExp } from "./L3-ast";
 import { parseL3Exp } from "./L3-ast";
 import { applyEnv, makeEmptyEnv, makeEnv, Env } from "./L3-env-sub";
-import { isClosure, makeClosure, Closure, Value, makeClassValue, isClassValue, ClassValue, isObjectValue, makeObjectValue} from "./L3-value"; //We added the last 2
+import { isClosure, makeClosure, Closure, Value } from "./L3-value";
 import { first, rest, isEmpty, List, isNonEmptyList } from '../shared/list';
 import { isBoolean, isNumber, isString } from "../shared/type-predicates";
 import { Result, makeOk, makeFailure, bind, mapResult, mapv } from "../shared/result";
@@ -17,7 +17,6 @@ import { applyPrimitive } from "./evalPrimitive";
 import { parse as p } from "../shared/parser";
 import { Sexp } from "s-expression";
 import { format } from "../shared/format";
-import exp from "constants";
 
 // ========================================================
 // Eval functions
@@ -38,8 +37,6 @@ const L3applicativeEval = (exp: CExp, env: Env): Result<Value> =>
                             (rands: Value[]) =>
                                 L3applyProcedure(rator, rands, env))) :
     isLetExp(exp) ? makeFailure('"let" not supported (yet)') :
-    isClassExp(exp) ? evalClass(exp, env) : //Added by us
-    isObjectValue(exp) ? evalObject(exp, env) : //Added by us
     makeFailure('Never');
 
 export const isTrueValue = (x: Value): boolean =>
@@ -58,24 +55,16 @@ const L3applyProcedure = (proc: Value, args: Value[], env: Env): Result<Value> =
     isClosure(proc) ? applyClosure(proc, args, env) :
     makeFailure(`Bad procedure ${format(proc)}`);
 
-// Added by us
-const evalClass = (exp: ClassExp, env: Env): Result<Object> =>
-    makeOk(makeObjectValue(makeClassValue(exp.fields, exp.methods), exp.fields));
-
-// Added by us
-const evalObject = (exp: ObjectExp)
-
 // Applications are computed by substituting computed
 // values into the body of the closure.
 // To make the types fit - computed values of params must be
 // turned back in Literal Expressions that eval to the computed value.
-const valueToLitExp = (v: Value): NumExp | BoolExp | StrExp | LitExp | PrimOp | ProcExp | ClassExp =>
+const valueToLitExp = (v: Value): NumExp | BoolExp | StrExp | LitExp | PrimOp | ProcExp =>
     isNumber(v) ? makeNumExp(v) :
     isBoolean(v) ? makeBoolExp(v) :
     isString(v) ? makeStrExp(v) :
     isPrimOp(v) ? v :
     isClosure(v) ? makeProcExp(v.params, v.body) :
-    isObjectValue(v) ? makeClassExp() (v) :
     makeLitExp(v);
 
 const applyClosure = (proc: Closure, args: Value[], env: Env): Result<Value> => {
--- L3/LexicalTransformations.ts
+++ L3/LexicalTransformations.ts
@@ -1,28 +1,14 @@
-import { map} from "ramda";
-import { ClassExp, ProcExp, Exp, Program, VarDecl, CExp, makeProcExp, makeIfExp, Binding, makeAppExp } from "./L3-ast";
+import { ClassExp, ProcExp, Exp, Program } from "./L3-ast";
 import { Result, makeFailure } from "../shared/result";
-import { first } from "../shared/list";
 
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: class2proc(classExp)
 Type: ClassExp => ProcExp
 */
-export const class2proc = (exp: ClassExp): ProcExp => {
-    const fields : VarDecl[] = exp.fields;
-    const vals : CExp[] = map((b) => b.val, exp.methods);
-    return makeProcExp (fields, makeBodyProc(exp.methods));
-}
-
-const makeBodyProc = (methods: Binding[]): CExp[] => {
-    const vars : VarDecl[] = map((b) => b.var, methods);
-    const vals : CExp[] = map((b) => b.val, methods);
-    return makeProcExp(c, vals[0]);
-}
-
-const makeBodyApp = ()
-
-// makeProcExp(var, makeIfExp(eq?var, val, ---))
+export const class2proc = (exp: ClassExp): ProcExp =>
+    //@TODO
+    exp;
 
 
 /*
